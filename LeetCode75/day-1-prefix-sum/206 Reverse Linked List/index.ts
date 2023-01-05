ba76439a86f6497cc814fe30bfada95d133dc6d8
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-01-04 21:43:18 -0300

    206 Reverse Linked List

---
+++ LeetCode75/day-1-prefix-sum/206 Reverse Linked List/index.ts
@@ -0,0 +1,28 @@
+
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+
+    let current: ListNode | null = head;
+    let prev: ListNode | null = null;
+
+    while (current) {
+
+        let next: ListNode | null = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+
+    }
+
+    return prev;
+
+};
