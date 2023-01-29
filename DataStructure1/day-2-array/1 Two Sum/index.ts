faf711bef15236d02ee18d642e6009a728fccc80
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-01-29 11:21:41 -0300

    Improved the 1. Two Sum exercise performance

--- DataStructure1/day-2-array/1 Two Sum/index.ts
+++ DataStructure1/day-2-array/1 Two Sum/index.ts
@@ -1,19 +1,21 @@
 function twoSum(nums: number[], target: number): number[] {
 
+    let map: Map<number, number> = new Map();
+
     for (let i = 0; i < nums.length; i++) {
 
-        for (let j = i + 1; j < nums.length; j++) {
+        let complementIndex: number | undefined = map.get(nums[i]);
 
-            if (nums[i] + nums[j] === target) {
+        if (complementIndex != undefined) {
 
-                return [i, j];
-
-            }
+            return [complementIndex, i];
 
         }
 
+        map.set(target - nums[i], i);
+
     }
 
-    return [];
+    return []
 
 };
