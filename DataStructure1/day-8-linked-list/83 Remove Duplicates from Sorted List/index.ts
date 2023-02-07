45885f39107734e05eadc72672121722dc753d1f
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-02-07 20:20:52 -0300

    Added the exercises from day 8 (Linked list)

---
+++ DataStructure1/day-8-linked-list/83 Remove Duplicates from Sorted List/index.ts
@@ -0,0 +1,26 @@
+
+//  Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+
+    let curr = head;
+
+    while (curr !== null && curr.next !== null) {
+        if (curr.next.val === curr.val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+
+    return head;
+
+};
